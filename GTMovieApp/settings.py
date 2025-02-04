183ab4a9cd0aecdc5375404853a79103b6136fad
“Isabelle <118849336+isb05@users.noreply.github.com>
2025-02-03 19:33:35 -0500

    textbook chapter 3

--- GTMovieApp/settings.py
+++ GTMovieApp/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'GTMovieApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'moviesstore/templates')],
+        'DIRS': [os.path.join(BASE_DIR,'gtmovieapp/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
