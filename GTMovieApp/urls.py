4216d330218bb125911f9fa1b77435e871421d20
“Isabelle <118849336+isb05@users.noreply.github.com>
2025-02-05 23:46:55 -0500

    user search movies story, IW

--- GTMovieApp/urls.py
+++ GTMovieApp/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('movies.urls')),
     path('', include('home.urls')),
+    path('movies/', include('movies.urls'))
+
 ]
+urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
+
 admin.site.site_header = "GTMovieApp Administration"
 admin.site.site_title = "GTMovieApp Administration"
 admin.site.index_title = "GTMovieApp Administration"
