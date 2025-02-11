91874f7537996d63a831518468698ea5173ff30c
“Isabelle <118849336+isb05@users.noreply.github.com>
2025-02-10 21:55:31 -0500

    adjusted some issues with movie reviews

--- movies/models.py
+++ movies/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
