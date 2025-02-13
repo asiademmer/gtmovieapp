ecb798b24889df93f0fcb4c0926661bac382e02f
Ariam Netsereab <98781007+AruNets@users.noreply.github.com>
2025-02-13 16:05:05 -0500

    Update tmdb.py

--- tmdb.py
+++ tmdb.py
@@ -6,27 +6,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # TMDB API configuration
-API_KEY = "6729c4cad6820ce8cfdfc6ffe135b332"  # Replace with your TMDB API key
+API_KEY = "6729c4cad6820ce8cfdfc6ffe135b332"
 BASE_URL = "https://api.themoviedb.org/3"
 
-def get_movies(page_limit=3):  # Fetch data from multiple pages (default: 3 pages)
+def get_movies(page_limit=3):  
     all_movies = []
-    for page in range(1, page_limit + 1):  # Loop through pages
+    for page in range(1, page_limit + 1):  
         endpoint = f"{BASE_URL}/movie/popular"
         params = {
             "api_key": API_KEY,
             "language": "en-US",
-            "page": page,  # Specify the page number
+            "page": page,  
         }
         response = requests.get(endpoint, params=params)
         if response.status_code == 200:
             movies = response.json().get("results", [])
-            all_movies.extend(movies)  # Add movies from this page to the list
+            all_movies.extend(movies) 
         else:
             print(f"Error fetching page {page}: {response.status_code}")
             break
 
-    # Fetch genres and map genre IDs to genre names
+
     genres = get_genres()
     for movie in all_movies:
         movie["genre_names"] = [genres.get(genre_id, "Unknown") for genre_id in movie.get("genre_ids", [])]
