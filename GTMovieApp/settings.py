0224254d6ab02dbb94094af4c392b40e88c06820
asiademmer <ademmer3@gatech.edu>
2025-02-17 21:03:47 -0500

    for gui
    # Conflicts:
    #	requirements.txt

--- GTMovieApp/settings.py
+++ GTMovieApp/settings.py
@@ -125,7 +125,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'GTMovieApp','static')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'gtmovieapp/static/',
